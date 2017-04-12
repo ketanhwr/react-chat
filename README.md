93f7a029aaaf97906c9584825b94f95a1c5c6cce
Ketan Gupta <ketan19972010@gmail.com>
2017-04-12 22:14:04 +0530

    Adds initial files

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Chat
+
+A simple chat application written in Node.js using Socket.io and React.
+
+# Setup
+
+- `npm install` to install dependencies.
+- `npm start` to start the application.
---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config.js');
+
+const port = 3000;
+const app = express();
+
+var server = require('http').Server(app);
+
+const io = require('socket.io').listen(server);
+
+const compiler = webpack(config);
+const middleware = webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'src',
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  }
+});
+
+app.use(middleware);
+app.use(webpackHotMiddleware(compiler));
+app.get('*', function response(req, res) {
+  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+  res.end();
+});
+
+server.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
+
+io.on('connection', function(socket){
+  console.log("client connected!");
+});
