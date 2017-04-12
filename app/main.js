0b7f54580c7a4ba8c302b96e9ff2c7d64318b570
Ketan Gupta <ketan19972010@gmail.com>
2017-04-12 23:10:18 +0530

    Adds message and messagelist classes

--- app/main.js
+++ app/main.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import styles from './App.css';
 
 var socket = io();
+
+class MessageBox extends Component {
+
+  render() {
+
+    return (
+
+      <div className={styles.messageBox}>
+        <span className={styles.messageText}> {this.props.text} </span>
+        <span className={styles.messageTime}> {this.props.time} </span>
+      </div>
+
+    );
+  }
+}
+
+class MessageList extends Component {
+
+	render() {
+
+    const listItems = this.props.messagelist.map((message, i) => message.text);
+
+		return (
+
+			<div className={styles.messageList}>
+				{listItems}
+			</div>
+
+		);
+	}
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {messages:[{'text':'hello', 'time':'0'}]};
+  }
+
+  render() {
+
+    return (
+
+      <div className={styles.app}>
+        Chat Application!
+        <MessageList messagelist={this.state.messages} />
+      </div>
+
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
